8d5d0330aa738ece0618f6415deb6083d27305db
dvokivon <dimanov13@gmail.com>
2022-03-04 00:57:50 +0300

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -1,23 +1,6 @@
 import requests
-url = input('host: ')
-user = input('login: ')
-passw = input('password: ')
-repo = input('choose repository to clean: ')
-vers_pr = input('version: ')
-name_pr =input('name: ')
-ids_to_delete=[]
-components_response = requests.get(url+'service/rest/v1/components?repository='+repo,auth=(user, passw))
-dict=components_response.json()
-for i in range (len(dict['items'])):
-    if dict['items'][i]['version'].find(vers_pr)>=0:
-        if name_pr != '':
-            if dict['items'][i]['name'].find(name_pr)>=0:
-                ids_to_delete.append(dict['items'][i]['id'])
-        else: ids_to_delete.append(dict['items'][i]['id'])
-continuationToken = dict['continuationToken']
-print(continuationToken)
-while True:
-    components_response = requests.get(url+'service/rest/v1/components?repository='+repo,auth=(user, passw),params={'continuationToken':continuationToken})
+
+def search_for_matches():
     dict=components_response.json()
     for i in range (len(dict['items'])):
         if dict['items'][i]['version'].find(vers_pr)>=0:
@@ -25,11 +8,28 @@ while True:
                 if dict['items'][i]['name'].find(name_pr)>=0:
                     ids_to_delete.append(dict['items'][i]['id'])
             else: ids_to_delete.append(dict['items'][i]['id'])
-    print('следующая страница')
     continuationToken = dict['continuationToken']
+    print(continuationToken)
+    return continuationToken, dict
+    
+url = input('host: ')
+user = input('login: ')
+passw = input('password: ')
+repo = input('choose repository to clean: ')
+vers_pr = input('version: ')
+name_pr =input('name: ')
+ids_to_delete=[]
+continuationToken = 0
+components_response = requests.get(url+'service/rest/v1/components?repository='+repo,auth=(user, passw))
+search_for_matches()
+print('next page')
+while True:
+    components_response = requests.get(url+'service/rest/v1/components?repository='+repo,auth=(user, passw),params={'continuationToken':continuationToken})
+    search_for_matches()  
+    print('next page')
     if continuationToken == None:
         break
 print(ids_to_delete)
-for id in ids_to_delete:
-    requests.delete(url+'service/rest/v1/components/'+id, auth=(user, passw))
-    print('Удалил)', id)
+#for id in ids_to_delete:
+ #   requests.delete(url+'service/rest/v1/components/'+id, auth=(user, passw))
+  #  print('i`ve deleted ', id)
